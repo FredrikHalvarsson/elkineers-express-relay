b09c7790e1ede43e90ad5e08d86bac76755507cc
Fredrik Halvarsson <Fredrik.Halvarsson@live.se>
2024-03-21 17:58:47 +0100

    updated server to use notion hq on all get- requests

--- notion/get/getData.js
+++ notion/get/getData.js
@@ -34,36 +34,30 @@ router.get('/timereports', async (req, res)=>{
     } 
 });
 //GET PROJECTS
-router.post('/projects', async (req, res)=>{
+router.get('/projects', async (req, res)=>{
     try 
     {
-     const response = await axios.post(`${process.env.NOTION_API_BASE_URL}/databases/${process.env.DATABASE_ID_PROJECTS}/query`, req.body,{
-        headers: {
-            'Authorization': `Bearer ${process.env.NOTION_TOKEN}`, 
-            'Notion-Version': '2022-06-28' 
-        }
-     });
-    res.json(response.data);    
-    } 
-    catch (error) 
+    const response = await notion.databases.query({
+        database_id: process.env.DATABASE_ID_PROJECTS
+    });
+    console.log('success')
+    res.send(response)
+    } catch (error) 
     { 
         console.log(error);
         res.status(500).json({message: 'Server error'});
     } 
 });
 //GET PEOPLE
-router.post('/people', async (req, res)=>{
+router.get('/people', async (req, res)=>{
     try 
     {
-     const response = await axios.post(`${process.env.NOTION_API_BASE_URL}/databases/${process.env.DATABASE_ID_PEOPLE}/query`, req.body,{
-        headers: {
-            'Authorization': `Bearer ${process.env.NOTION_TOKEN}`, 
-            'Notion-Version': '2022-06-28' 
-        }
-     });
-    res.json(response.data);    
-    } 
-    catch (error) 
+    const response = await notion.databases.query({
+        database_id: process.env.DATABASE_ID_PEOPLE
+    });
+    console.log('success')
+    res.send(response)
+    } catch (error) 
     { 
         console.log(error);
         res.status(500).json({message: 'Server error'});
@@ -71,3 +65,60 @@ router.post('/people', async (req, res)=>{
 });
 
 module.exports = router
+
+//*********************** -Legacy Code- **************************** */
+//GET TIMEREPORTS
+// router.post('/timereports', async (req, res)=>{
+//     try 
+//     {
+//      const response = await axios.post(`${process.env.NOTION_API_BASE_URL}/databases/${process.env.DATABASE_ID_TIMEREPORTS}/query`, req.body,{
+//         headers: {
+//             'Authorization': `Bearer ${process.env.NOTION_TOKEN}`, 
+//             'Notion-Version': '2022-06-28' 
+//         }
+//      });
+//     res.json(response.data);    
+//     } 
+//     catch (error) 
+//     { 
+//         console.log(error);
+//         res.status(500).json({message: 'Server error'});
+//     } 
+// });
+// //GET PROJECTS
+// router.post('/projects', async (req, res)=>{
+//     try 
+//     {
+//      const response = await axios.post(`${process.env.NOTION_API_BASE_URL}/databases/${process.env.DATABASE_ID_PROJECTS}/query`, req.body,{
+//         headers: {
+//             'Authorization': `Bearer ${process.env.NOTION_TOKEN}`, 
+//             'Notion-Version': '2022-06-28' 
+//         }
+//      });
+//     res.json(response.data);    
+//     } 
+//     catch (error) 
+//     { 
+//         console.log(error);
+//         res.status(500).json({message: 'Server error'});
+//     } 
+// });
+// //GET PEOPLE
+// router.post('/people', async (req, res)=>{
+//     try 
+//     {
+//      const response = await axios.post(`${process.env.NOTION_API_BASE_URL}/databases/${process.env.DATABASE_ID_PEOPLE}/query`, req.body,{
+//         headers: {
+//             'Authorization': `Bearer ${process.env.NOTION_TOKEN}`, 
+//             'Notion-Version': '2022-06-28' 
+//         }
+//      });
+//     res.json(response.data);    
+//     } 
+//     catch (error) 
+//     { 
+//         console.log(error);
+//         res.status(500).json({message: 'Server error'});
+//     } 
+// });
+// module.exports = router
